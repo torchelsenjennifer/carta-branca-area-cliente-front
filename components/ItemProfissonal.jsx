0c6f03f4d4f7566505b6f4d897b7e2d40c808635
Jennifer <jennifertorchelsen@gmail.com>
2023-10-29 19:36:11 -0300

    ✨ ┇ adicionado cards de profissionais

---
+++ components/ItemProfissonal.jsx
@@ -0,0 +1,14 @@
+export default function ItemProfissional(props){
+	return(
+		<div class="col">
+			<div className="card">
+				<img src={props.profissional.imagem} className="card-img-top" alt="profissional"/>
+				<div className="card-body">
+					<h5 className="card-title">{props.profissional.titulo}</h5>
+					<p className="card-text">{props.profissional.nome} - {props.profissional.especialidade}</p>
+					<p className="small">{props.profissional.especialidade}</p>
+				</div>
+    		</div>
+		</div>
+	)
+}
