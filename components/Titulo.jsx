a8f3bd0781fcb9c20c3cacfc00a7da360088e689
Cristoffer Rodrigues Laner <cristofferrlaner@gmail.com>
2023-10-29 22:07:46 -0300

    ✨ ┇ Implementado mudanças no título ao fazer login

--- components/Titulo.jsx
+++ components/Titulo.jsx
@@ -1,6 +1,19 @@
+'use client'
 import Link from "next/link";
+import { useContext } from "react";
+import { ClienteContext } from "@/contexts/cliente";
 
 export default function Titulo() {
+
+  const { clienteNome, mudaId, mudaNome } = useContext(ClienteContext)
+
+  function logout() {
+    if (confirm("Confirma saída do sistema?")) {
+      mudaId(null)
+      mudaNome("")
+    }
+  }
+
   return (
     <nav className="navbar bg-dark">
       <div className="row container-fluid">
@@ -13,21 +26,28 @@ export default function Titulo() {
               height="48"
               className="d-inline-block align-text-top float-start"
             />
-            <h3 className="float-start mt-3 ms-2">Estabelecimento Carta Branca</h3>
+            <h3 className="float-start mt-2 ms-2">Studio Carta Branca</h3>
           </Link>
         </div>
-		<div className="col text-white">
-			<div className="input-group mb-3 mt-2">
-				<input type="text" className="form-control" placeholder="Profissional" aria-label="Recipient's username" aria-describedby="button-addon2" />
-				<button className="btn bg-danger" type="button" id="button-addon2">Pesquisar</button>
-			</div>
-		</div>
-		<div className="col">
-			<Link href="/login" className="text-white">
-				<h4 className="float-end">Identifique-se</h4>
-				<i className="bi bi-person-fill-up text-white ms-2 float-end fs-4"></i>
-			</Link>
-		</div>
+        <div className="col text-white">
+          <div className="input-group mb-3 mt-3">
+            <input type="text" className="form-control" placeholder="Profissional" aria-label="Recipient's username" aria-describedby="button-addon2" />
+            <button className="btn bg-danger" type="button" id="button-addon2">Pesquisar</button>
+          </div>
+        </div>
+        <div className="col">
+          {clienteNome == "" ?
+            <Link href="/login" className="text-white">
+              <i className="bi bi-person-fill-up text-white ms-2 me-2 float-end fs-4"></i>
+              <h4 className="float-end">Identifique-se</h4>
+            </Link>
+            :
+            <div className="text-white" onClick={logout}>
+              <i className="bi bi-person-fill-down text-white ms-2 me-2 float-end fs-4"></i>
+              <h4 className="float-end">{clienteNome}</h4>
+            </div>
+          }
+        </div>
       </div>
     </nav>
   );
