7794dc17468396b6b51d433d2776c18f78b53e2c
Cristoffer Rodrigues Laner <cristofferrlaner@gmail.com>
2023-10-29 22:07:14 -0300

    🩹 ┇ Correção duplicidade de informações na listagem dos profissionais

--- components/ItemProfissional.jsx
+++ components/ItemProfissional.jsx
@@ -1,14 +1,14 @@
-export default function ItemProfissional(props){
-	return(
+export default function ItemProfissional(props) {
+	return (
 		<div className="col">
 			<div className="card">
-				<img src={props.profissional.imagem} className="card-img-top" alt="profissional"/>
+				<img src={props.profissional.imagem} className="card-img-top" alt="profissional" />
 				<div className="card-body">
 					<h5 className="card-title">{props.profissional.titulo}</h5>
-					<p className="card-text">{props.profissional.nome} - {props.profissional.especialidade}</p>
+					<p className="card-text">{props.profissional.nome}</p>
 					<p className="small">{props.profissional.especialidade}</p>
 				</div>
-    		</div>
+			</div>
 		</div>
 	)
 }
