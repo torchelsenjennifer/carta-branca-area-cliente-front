5aa5c7a4c6738200d3e24f922020660ec5875314
Jennifer <jennifertorchelsen@gmail.com>
2023-11-02 18:23:06 -0300

    ✨ ┇ ajuste de Indentação

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,7 @@
 export default function Home() {
-	return (
-
-		<div className="relative">
-			<img src="./cartaBranca.png" alt="Logo" className="img-fluid" />
-		</div>
-
-	);
-  }
+  return (
+    <div className="relative">
+      <img src="./cartaBranca.png" alt="Logo" className="img-fluid" />
+    </div>
+  );
+}
