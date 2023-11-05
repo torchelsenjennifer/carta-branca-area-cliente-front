135bcc99c9903dfada14630dde3dcfa2caaaf57f
Cristoffer Rodrigues Laner <cristofferrlaner@gmail.com>
2023-11-05 20:51:01 -0300

    💩 ┇ Type erro adjusted

--- components/Estrelas.jsx
+++ components/Estrelas.jsx
@@ -4,12 +4,12 @@ export default function Estrelas({ soma, num }) {
   const estrelas = [];
 
   for (let i = 1; i <= media; i++) {
-    estrelas.push(<i class="bi bi-star-fill text-warning"></i>);
+    estrelas.push(<i className="bi bi-star-fill text-warning"></i>);
   }
   if (media % 1 > 0.25 && media % 1 <= 0.75) {
-    estrelas.push(<i class="bi bi-star-half text-warning"></i>);
+    estrelas.push(<i className="bi bi-star-half text-warning"></i>);
   } else if (media % 1 > 0.75) {
-    estrelas.push(<i class="bi bi-star-fill text-warning"></i>);
+    estrelas.push(<i className="bi bi-star-fill text-warning"></i>);
   }
 
   return <div className="float-start">{estrelas}</div>;
